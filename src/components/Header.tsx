1eea2326933bc924be1d54ca712c317cc13b1978
Fad Rahim <fadrahim1@gmail.com>
2025-07-18 23:25:00 +0800

    Added in link to app in sign in

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -76,7 +76,7 @@ function MobileNavigation() {
         <MobileNavLink href="#testimonials">Testimonials</MobileNavLink>
         <MobileNavLink href="#pricing">Pricing</MobileNavLink>
         <hr className="m-2 border-slate-300/40" />
-        <MobileNavLink href="/login">Sign in</MobileNavLink>
+        <MobileNavLink href="https://app.geteditions.com">Sign in</MobileNavLink>
       </PopoverPanel>
     </Popover>
   )
@@ -99,7 +99,7 @@ export function Header() {
           </div>
           <div className="flex items-center gap-x-5 md:gap-x-8">
             <div className="hidden md:block">
-              <NavLink href="/login">Sign in</NavLink>
+              <NavLink href="https://app.geteditions.com">Sign in</NavLink>
             </div>
             <Button href="/register" color="blue">
               <span>
